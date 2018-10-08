5f0fbbe5460085c3511bfe8f6e58d5cc91699f7d
jonassuncao <jaa020399@gmail.com>
2018-10-08 15:08:47 -0300

    Adicionando Abas "Despesas a Compensar" e "Carteira de Cobrança"

--- Views/Sistema/admin/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js
+++ Views/Sistema/admin/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js
@@ -1722,11 +1722,11 @@
 	$.fn.datepicker.Constructor = Datepicker;
 	var dates = $.fn.datepicker.dates = {
 		en: {
-			days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-			daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-			daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
-			months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-			monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+			days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+			daysShort: [ "Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab" ],
+			daysMin: [ "Dm","Se","Te","Qa","Qi","Sx","Sa" ],
+			months: ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro" ],
+			monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
 			today: "Today",
 			clear: "Clear",
 			titleFormat: "MM yyyy"
