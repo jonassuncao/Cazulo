5cd961b5c6235c0a6d59d127d7a0d884b333f0e9
Dominic <dominic.rp.s2@gmail.com>
2018-10-16 01:02:57 -0300

    Atualização do login, alteração na tabela de despesas

--- Views/Sistema/admin/js/script_js.js
+++ Views/Sistema/admin/js/script_js.js
@@ -11,26 +11,28 @@ function ativarInput(idInput){
 
 //atualizar os ids quando remover algum
 //Quando//Quando clicar no Botao da despesa  
-$(document).on('click', '#bot', function(){
-    var valor = $('#valor').val();
+$(document).on('click', '#adicionar-item', function(){
+    var valorDespesa = $('#valor').val();
     var desc = $('#descricao').val();
-    var desp = $('').val;
-    var combo;
+    var comboBox;
     var novoID;
-    var soma = 0;
+    var subTotal = 0;
+    var inputFundoReserva = $('#inputValorFdReserva').val();
+    var fundoReserva;
+    var total;
+   
+    $("#combo option:selected").each(function() {
+        comboBox = $(this).val();
+    });
 
     if(!$('#bodyTable tr:last td').length) novoID = 1;
     else novoID = parseInt($('#bodyTable tr:last td').html())+1;
-   
-    $("#combo option:selected").each(function() {
-     combo = $(this).val();
-    });
-    //adicionando linha
+    //adicionando linha na tabela
     $('#bodyTable').append(
         '<tr class="demo" id="tr_'+novoID+'">'                                     +
         '   <td>'+novoID+'</td>'                                                   +
-        '   <td>'+combo+'</td>'                                                    +
-        '   <td class="valor-unit-desp">'+valor+'</td>'                                                    +
+        '   <td>'+comboBox+'</td>'                                                    +
+        '   <td class="valor-unit-desp">'+valorDespesa+'</td>'                            +
         '   <td>'+desc+'</td>'                                                     +
         '   <td>'                                                                  +
         '       <i class="glyphicon glyphicon-pencil"></i>'                        +
@@ -39,7 +41,69 @@ $(document).on('click', '#bot', function(){
         '       </button>'                                                         +         
         '   </td>'                                                                 +
         '</tr>');
-    //adicionando o modal
+    //adicionando o modal da tabela para exclusão
+    $('<div class="modal fade" id="myConfig'+novoID+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">'+
+      '<div class="modal-dialog" role="document">'                                 +
+      '  <div class="modal-content">'                                              +
+      '    <div class="modal-body">'                                               +
+      '       <p>Tem certeza que deseja excluir este item?</p>'                    +
+      '    </div>'                                                                 +
+      '    <div class="modal-footer">'                                             +
+      '      <button id="lixo_'+novoID+'" type="button" onClick="excluirLinha(\'#tr_'+novoID+', #myConfig'+novoID+', .in\');" class="btn btn-danger modal_confirma" data-dismiss="modal" >'+
+      '        Sim'                                                                +
+      '      </button>'                                                            +
+      '      <button type="button" data-dismiss="modal" class="btn btn-default">'  +
+      '         Não'                                                               +
+      '      </button>'                                                            +
+      '    </div>'                                                                 +
+      '  </div>'                                                                   +
+      '</div>'                                                                     +
+      '</div>'                                                                     +
+      '</div>').insertBefore('table');
+
+    $('#footTable').css("visibility", "visible");
+      
+    //Soma do total
+    $('.table #bodyTable .valor-unit-desp').each(function () {
+        subTotal += parseFloat($(this).text()); 
+        $("#subTotal").html(subTotal);           
+    }); 
+
+    //Subtração Total
+    $(document).on('click', '.modal_confirma', function(){
+        subTotal -= valorDespesa;
+        $("#subTotal").html(subTotal);
+    }); 
+    //calcula o valor do fundo de reserva
+    fundoReserva = (subTotal * inputFundoReserva)/100;
+
+    $("#fundoReserva").html(fundoReserva); 
+
+    $('#total').html(fundoReserva+subTotal);
+
+}); 
+//Excluir a linha da tabela quando é confirmado o modal de exclusão
+function excluirLinha(linha){
+    var subTotal = $("#subTotal").val();
+
+    ///var text=$(linha).html(id);
+
+    $(linha).remove();
+
+    //alert(text);
+
+}
+
+//calendário
+$('#data').datepicker("setDate", new Date()).datepicker({
+    format: 'mm/yyyy',
+    language: "pt-BR",
+    autoclose: true,
+    defaultViewDate: true,
+    assumeNearbyYear: true,    
+});
+
+$(document).on('click', '.modal_confirma', function(){
     $('<div class="modal fade" id="myConfig'+novoID+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">'+
       '<div class="modal-dialog" role="document">'                                 +
       '  <div class="modal-content">'                                              +
@@ -57,30 +121,36 @@ $(document).on('click', '#bot', function(){
       '  </div>'                                                                   +
       '</div>'                                                                     +
       '</div>'                                                                     +
-      '</div>').insertBefore('table');
-      
-    //Soma do total
-    $('.table #bodyTable .valor-unit-desp').each(function () {
-        soma += parseFloat($(this).text()); 
-        $("#soma-desp").text(soma);           
+      '</div>').insertBefore('table');   
+});
+
+$(document).on('click', '#select-itens', function(){
+
+    $('.table thead tr').prepend('<th>kkkkkkkkkkk</th>');
+
+    $('.table tbody .demo').prepend(
+        '<div class="form-group">'+
+        '    <label>'+
+        '       <input type="checkbox" class="minimal">'+
+        '    </label>'+
+        '</div>');
+    
+});
+
+$(document).on('click', '#salvarConfFdReserva', function(){
+    var inputFundoReserva = $('#inputValorFdReserva').val();
+    var subTotal = $("#subTotal").html();
+    var fundoReserva;
+    var total;
+
+    //Não fecha o modal**************************
+    //$('#myConfig').modal('hide').noConflict(); 
+    fundoReserva = (subTotal * inputFundoReserva)/100;
+    total = subTotal + fundoReserva;
+
+    $("#fundoReserva").html(fundoReserva); 
+    //ERRADO, não atualiza o total quando coloco o fundo de reserva********************
+    $(document).on('click', '#salvarConfFdReserva', function(){
+        $('#total').val(total);
     });
-    //Subtração Total
-    $(document).on('click', '.modal_confirma', function(){
-        soma -= valor;
-        $("#soma-desp").text(soma);
-    }); 
-
-}); 
-//Excluir a linha da tabela quando é confirmado o modal de exclusão
-function excluirLinha(linha){
-    $(linha).remove();
-}
-
-//calendário
-$('#data').datepicker("setDate", new Date()).datepicker({
-    format: 'mm/yyyy',
-    language: "pt-BR",
-    autoclose: true,
-    defaultViewDate: true,
-    assumeNearbyYear: true,    
 });
